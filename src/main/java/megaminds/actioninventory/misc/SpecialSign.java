9e124455c1c69e8f96f063ebdc14aa6635317de5
Hoid2 <61083843+Hoid2@users.noreply.github.com>
2022-01-17 08:25:19 -0500

    Remove Dev files
    Unused at the moment

--- src/main/java/megaminds/actioninventory/misc/SpecialSign.java
+++
@@ -1,9 +0,0 @@
-package megaminds.actioninventory.misc;
-
-public interface SpecialSign {
-	boolean isSpecialSign();
-	void setSpecialSign(boolean isSpecial);
-	
-	String getSpecial();
-	void setSpecial(String name);
-}
