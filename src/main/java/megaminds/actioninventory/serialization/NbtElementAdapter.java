aeb709c4a2cc6ccbe3ecbbb27d5f1a694d80872d
Jacob <61083843+Hoid2@users.noreply.github.com>
2022-03-08 21:44:49 -0500

    Fix Empty NbtData Error
    (hopefully)

--- src/main/java/megaminds/actioninventory/serialization/NbtElementAdapter.java
+++ src/main/java/megaminds/actioninventory/serialization/NbtElementAdapter.java
@@ -17,10 +17,13 @@ public class NbtElementAdapter extends TypeAdapter<NbtElement> {
 	public void write(JsonWriter out, NbtElement value) throws IOException {
 		out.value(new StringNbtWriter().apply(value));
 	}
+
 	@Override
 	public NbtElement read(JsonReader in) throws IOException {
+		var s = in.nextString();
+		if (s.isEmpty()) return null;
 		try {
-			return new StringNbtReader(new StringReader(in.nextString())).parseElement();
+			return new StringNbtReader(new StringReader(s)).parseElement();
 		} catch (CommandSyntaxException e) {
 			throw new IllegalArgumentException(e);
 		}
